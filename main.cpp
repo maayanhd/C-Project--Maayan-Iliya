8135bc3eb3e171de90430db79cfa8c0815100c51
Maayan <maayan.hadarwork@gmail.com>
2020-01-16 19:06:39 +0200

    1

--- main.cpp
+++ main.cpp
@@ -5,16 +5,10 @@
 
 int main()
 {
+	Menu menu;
+	bool exit = false;
+	while (!exit)
 	{
-		Menu menu;
-		bool exit = false;
-		while (!exit)
-		{
-			menu.show(exit);
-		}
+		menu.show(exit);
 	}
-	if (_CrtDumpMemoryLeaks())
-		cout << "ass holes!\n";
-	else
-		cout << "awesome man!\n";
 }
